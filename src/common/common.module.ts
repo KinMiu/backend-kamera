708e28de2c14578d22d4f80141ee6bdcd37e694e
KinMiu <prasetyoaldi875@gmail.com>
2026-09-04 10:21:24 +0700

    fix error messageSerive yang dikonfigurasi dengan scope REQUEST

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,5 @@
 import { Global, Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { MessageService } from './services/message.service';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
@@ -8,15 +9,16 @@ import { HttpLoggerMiddleware } from './middleware/http-logger.middleware';
 @Module({
   providers: [
     MessageService,
-    ResponseInterceptor,
-    AllExceptionsFilter,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
     HttpLoggerMiddleware,
   ],
-  exports: [
-    MessageService,
-    ResponseInterceptor,
-    AllExceptionsFilter,
-    HttpLoggerMiddleware,
-  ],
+  exports: [MessageService, HttpLoggerMiddleware],
 })
 export class CommonModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
 import { initializeTransactionalContext } from 'typeorm-transactional';
-import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
-import { ResponseInterceptor } from './common/interceptors/response.interceptor';
-import { MessageService } from './common/services/message.service';
 
 async function bootstrap() {
   // Initialize typeorm-transactional context (LSKK Section 1)
   initializeTransactionalContext();
 
   const app = await NestFactory.create(AppModule);
-
-  // Centralized Exception Filter (LSKK Section 2)
-  const httpAdapterHost = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
-
-  // Response Wrapper Interceptor (LSKK Section 4)
-  const messageService = app.get(MessageService);
-  app.useGlobalInterceptors(new ResponseInterceptor(messageService));
 
   // CORS Configuration (LSKK Section 1)
   app.enableCors({
